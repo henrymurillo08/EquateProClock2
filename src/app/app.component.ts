225ad8fda4008783c909474db435baa55af47b23
henrymurillo08 <henrymurillo2014@gmail.com>
2018-09-28 17:25:18 -0600

    boton para atras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { Storage } from '@ionic/storage'
+import { Storage } from '@ionic/storage';
+
 
 import { VerificacionPage } from '../pages/verificacion/verificacion';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -28,8 +29,13 @@ export class MyApp {
     })
       statusBar.styleDefault();
       splashScreen.hide();
+      platform.registerBackButtonAction(fn=>{
+        platform.exitApp();
+      })
     });
     
   }
+
+  
 }
 
